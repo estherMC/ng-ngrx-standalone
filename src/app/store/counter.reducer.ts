15362be212d28aa3a86e58344fad69eacf936895
Esther Martorell <esther_3@hotmail.com>
2023-08-09 08:18:45 +0200

    :sparkles: alternative way to create a reducer

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -3,6 +3,12 @@ import { createReducer } from "@ngrx/store";
 //this initialState pot ser un objecte, un string, un boolean, un number...
 const initialState = 0;
 
-export const counterReducer = createReducer(
-    initialState
-);
+// export const counterReducer = createReducer(
+//     initialState
+// );
+
+//Alternative to use (under the hood creates the same):
+//S'executarà al inici i quan s'executi una acció
+export function counterReducer(state = initialState) {
+    return state;
+}
