08aa15213b5d3fea7610222f9ed724db2490fc12
Esther Martorell <esther_3@hotmail.com>
2023-08-09 09:01:58 +0200

    :sparkles: alternative way to create reducer

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -4,14 +4,17 @@ import { increment } from "./counter.actions";
 //this initialState pot ser un objecte, un string, un boolean, un number...
 const initialState = 0;
 
-export const counterReducer = createReducer(
-    initialState,
-    //Important: sempre crear un objecte nou, no fer mutate
-    on(increment, (state, action) => state + action.value)
-);
+// export const counterReducer = createReducer(
+//     initialState,
+//     //Important: sempre crear un objecte nou, no fer mutate
+//     on(increment, (state, action) => state + action.value)
+// );
 
 //Alternative to use (under the hood creates the same):
 //S'executarà al inici i quan s'executi una acció
-// export function counterReducer(state = initialState) {
-//     return state;
-// }
+export function counterReducer(state = initialState, action: any) {
+    if (action.type === '[Counter] Increment'){
+        return state + action.value;
+    }
+    return state;
+}
