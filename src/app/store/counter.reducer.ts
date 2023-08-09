11cec67474b65550fec2e8c3df847867869b7577
Esther Martorell <esther_3@hotmail.com>
2023-08-09 09:45:47 +0200

    :recycle: enable the first option

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,20 +1,21 @@
 import { Action, createReducer, on } from "@ngrx/store";
-import { CounterActions, INCREMENT, IncrementAction } from "./counter.actions";
+// import { CounterActions, INCREMENT, IncrementAction } from "./counter.actions";
+import { increment } from "./counter.actions";
 
 //this initialState pot ser un objecte, un string, un boolean, un number...
 const initialState = 0;
 
-// export const counterReducer = createReducer(
-//     initialState,
-//     //Important: sempre crear un objecte nou, no fer mutate
-//     on(increment, (state, action) => state + action.value)
-// );
+export const counterReducer = createReducer(
+    initialState,
+    //Important: sempre crear un objecte nou, no fer mutate
+    on(increment, (state, action) => state + action.value)
+);
 
 //Alternative to use (under the hood creates the same):
 //S'executarà al inici i quan s'executi una acció
-export function counterReducer(state = initialState, action: CounterActions | Action) {
-    if (action.type === INCREMENT){
-        return state + (action as IncrementAction).value;
-    }
-    return state;
-}
+// export function counterReducer(state = initialState, action: CounterActions | Action) {
+//     if (action.type === INCREMENT){
+//         return state + (action as IncrementAction).value;
+//     }
+//     return state;
+// }
